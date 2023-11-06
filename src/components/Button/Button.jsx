e51d46ba6075fd5dbe251f8a4255646dd1674fe6
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-06 19:12:13 +0200

    a

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import { StyledLoadMoreBtn } from './Button.styled';
 export const Button = ({ loadMore }) => {
   return (
     <StyledLoadMoreBtn type="button" onClick={loadMore}>
-      Load more
+      Load
     </StyledLoadMoreBtn>
   );
 };
